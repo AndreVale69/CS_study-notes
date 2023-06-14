ee9e95aa48512c6cd196e1d1e3fe1b59fb2f5f9e
AndreVale69 <57899285+AndreVale69@users.noreply.github.com>
2023-06-14 03:38:58 -0700

    Add files via upload

--- Tesina - FaaS/code/azure.py
+++ Tesina - FaaS/code/azure.py
@@ -6,20 +6,9 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        sum = req_body["val1"] + req_body["val2"]
-        return func.HttpResponse(
-             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.\nIl risultato e': {sum}",
-             status_code=200
-        )
+    req_body = req.get_json()
+    sum = req_body["val1"] + req_body["val2"]
+    return func.HttpResponse(
+            f"This HTTP triggered function executed successfully.\nIl risultato e': {sum}",
+            status_code=200
+    )
