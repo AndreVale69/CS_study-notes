d2ba0a9e76eeed764de74add16399c48df391f98
AndreVale69 <57899285+AndreVale69@users.noreply.github.com>
2023-06-18 14:20:15 -0700

    Delete Programmazione e sicurezza delle reti/esercizi/Client-server-socket directory

--- Programmazione e sicurezza delle reti/esercizi/Client-server-socket/Modifca/exec_client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# gcc network.c clientUDP.c -o clientUDP -lpthread
-# gcc network.c clientUDP_inc.c -o clientUDP -lpthread
-gcc network.c clientTCP.c -o clientTCP -lpthread
-# ./clientUDP
-./clientTCP
--- Programmazione e sicurezza delle reti/esercizi/Client-server-socket/Modifca/exec_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# gcc network.c serverUDP.c -o serverUDP -lpthread
-# gcc network.c serverUDP_inc.c -o serverUDP -lpthread
-gcc network.c serverTCP.c -o serverTCP -lpthread
-# ./serverUDP
-./serverTCP
